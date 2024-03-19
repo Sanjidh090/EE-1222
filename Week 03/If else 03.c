1f7c0d0054e8ce5d6a0fe9cfe31e6c9176ac8efd
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2024-03-19 17:53:08 +0600

    Add files via upload

---
+++ Week 03/If else 03.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+main() {
+int num = 8;
+switch (num) {
+case 7:
+    printf("value is 7");
+    break;
+case 8:
+    printf("value is 8");
+    break;
+default:
+    printf("Out of range");
+    break;
+}
+return 0;
+}
---
+++ Week 03/if else 04.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    goto AC;
+    printf("programming");
+    AC:
+        printf("Hello");
+
+}
