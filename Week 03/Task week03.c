68db3e301261355fd5b65a19c1a324f1dfde56d4
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2024-04-21 02:38:29 +0600

    Update Task week03.c

--- Week 03/Task week03.c
+++ Week 03/Task week03.c
@@ -5,17 +5,18 @@ int main() {
     printf("Enter marks: ");
     scanf("%f", &mark);
 
-    if (mark < 0) {
-        printf("Invalid mark");
-    } else if (mark <= 300 && mark >= 240) {
+    if (mark >= 240 && mark <= 300) {
         printf("First Class\n");
-    } else if (mark <= 239 && mark >= 180) {
-        printf("Second class\n");
-    } else if (mark <= 179 && mark >= 120) {
+    } else if (mark >= 180) {
+        printf("Second Class\n");
+    } else if (mark >= 120) {
         printf("Third Class\n");
-    } else if (mark < 120) {
+    } else if (mark >= 0) {
         printf("Fail\n");
+    } else {
+        printf("Invalid mark\n");
     }
 
     return 0;
 }
+
