3b9c37fb3e70a20197ea7063f629cd6a23581903
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2024-05-31 12:54:28 +0600

    task01.c

--- Week 07/task01.c
+++ Week 07/task01.c
@@ -1,2 +1,12 @@
 //replace value a=6,b=5 to previous version a=5,b=6
-//
+#include<stdio.h>
+int main()
+{
+    int a =6,b =5;
+    int *p =&a,*q =&b;
+    int temp;
+    temp = *p;
+    *p =*q; *q=temp;
+    printf("Value of a is %d \n",a);
+    printf("Value of b is %d ",b);
+}
